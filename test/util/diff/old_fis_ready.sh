e42bcbbc9fb04254fa97b4a94e9834a7f6be3ae6
tianlili <tianlili@baidu.com>
2013-10-10 17:41:11 +0800

    [change]修改项目名称fis_2.0_pc_diff

--- test/util/diff/old_fis_ready.sh
+++ test/util/diff/old_fis_ready.sh
@@ -1,6 +1,6 @@
 #!/home/work/.jumbo/bin/expect
 
-spawn su - fis -c "sh /home/work/repos/fis2_pc_diff/test/util/diff/release.sh old"
+spawn su - fis -c "sh /home/work/repos/fis_2.0_pc_diff/test/util/diff/release.sh old"
 expect "Password:"
 send cqmyg123\r
 expect eof
--- test/util/diff/start.sh
+++ test/util/diff/start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-TEST_PATH=/home/work/repos/fis2_pc_diff/test/util/diff
+TEST_PATH=/home/work/repos/fis_2.0_pc_diff/test/util/diff
 cd  ${TEST_PATH}
 if [ -f "result/report.xml" ];then
    rm "result/report.xml"
 fi
 sh product_code.sh
 sh new_fis_ready.sh
+chmod +x old_fis_ready.sh
 ./old_fis_ready.sh
 rm -rf result/*.html
 php -f diffall.class.php
