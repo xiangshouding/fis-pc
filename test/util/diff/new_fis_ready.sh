12034d71dbb4feb8f6496a17ceba97a754dd0f91
tianlili <tianlili@baidu.com>
2013-10-10 18:53:35 +0800

    [change]微调

--- test/util/diff/new_fis_ready.sh
+++ test/util/diff/new_fis_ready.sh
@@ -5,4 +5,5 @@ FISP_PATH=/home/work/repos/fis2.0_pc/
 cd ${FISP_PATH}                                                                                                                                npm install
 npm install
 npm install fis-preprocessor-inline
+npm install fis-preprocessor-image-set
 sh ${TEST_PATH}/release.sh new
